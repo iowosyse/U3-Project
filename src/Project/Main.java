53fb1c9755bde5d8b52f11aae0049e946546abcd
iowosyse <iowosyse@gmail.com>
2024-04-10 17:09:17 -0600

    v2.2.1

--- src/Project/Main.java
+++ src/Project/Main.java
@@ -3,6 +3,7 @@ package Project;
 import Controllers.Menus.TitleScreen;
 import Repositories.Seeder;
 
+/**@author Cándido Ortega Martínez */
 public class Main {
     public static void main(String[] args) {
         Seeder.initialize();
